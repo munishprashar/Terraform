8f631eae06dfbec1b4a55bda8f9752544e30b640
John Cena <John@gmail.com>
2021-04-07 23:42:43 -0600

    origin main

---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "location" {
+    type = string
+    default = "westus"
+}
+variable "prefix" {
+    type = string
+    default = "demo"
+}
+variable "ssh-source-address" {
+    type = string
+    default = "*"
+}
